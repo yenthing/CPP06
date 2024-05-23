822e5f650ad6bac617ca3dad133d1a0f0ba31e4d
yenthing <yenguyen1806@gmail.com>
2024-05-23 12:10:23 +0200

    still ex00 arg

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <limits>
+
+class ScalarConverter
+{
+    public:
+        ScalarConverter();
+        ScalarConverter(const std::string &str);
+        ScalarConverter(const ScalarConverter &other);
+        ~ScalarConverter();
+
+        ScalarConverter &operator=(const ScalarConverter &other);
+
+        void convert();
+        void print() const;
+
+    private:
+        void toChar();
+        void toInt();
+        void toFloat();
+        void toDouble();
+
+        std::string _str;
+        char _char;
+        int _int;
+        float _float;
+        double _double;
+        bool _isInf;
+        bool _isNan;
+        bool _isChar;
+        bool _isInt;
+        bool _isFloat;
+        bool _isDouble;
+};
